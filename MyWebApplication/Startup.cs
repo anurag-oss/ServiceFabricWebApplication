b99e8800814ebcc9aa309cc96a5ef67ab5358277
Nicolas Guelpa <nicolas@guelpa.me>
2017-12-11 17:27:29 -0500

    Update Startup.cs
    remove commented out code

--- MyWebApplication/Startup.cs
+++ MyWebApplication/Startup.cs
@@ -27,15 +27,15 @@ namespace MyWebApplication
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
+            if (env.IsDevelopment())
+            {
                 app.UseDeveloperExceptionPage();
                 app.UseBrowserLink();
-            //}
-            //else
-            //{
-            //    app.UseExceptionHandler("/Home/Error");
-            //}
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
 
             app.UseStaticFiles();
 
